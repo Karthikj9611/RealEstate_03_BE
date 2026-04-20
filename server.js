25480ec24a5a0134bc6ebacf523696803b44d8d0
Karthikj9611 <87406071+Karthikj9611@users.noreply.github.com>
2026-04-20 16:20:53 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -73,4 +73,16 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+
+app.get("/api/users", async (req, res) => {
+  try {
+    const users = await User.find().sort({ createdAt: -1 });
+    res.json(users);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json([]);
+  }
+});
+
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
